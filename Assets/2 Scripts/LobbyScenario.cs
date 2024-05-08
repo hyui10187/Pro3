805625bb4697e6f55df030708b26e687b3d09f20
Juklim <hyui10187@naver.com>
2024-05-08 23:07:40 +0900

    로비 화면에 유저 닉네임 표시하는 기능추가

---
+++ Assets/2 Scripts/LobbyScenario.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyScenario : MonoBehaviour {
+
+    [SerializeField]
+    private UserInfo user;
+
+    private void Awake() {
+        user.GetUserInfoFromBackend();
+    }
+    
+    
+}
--- Assets/2 Scripts/UserInfo.cs
+++ Assets/2 Scripts/UserInfo.cs
@@ -1,18 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using BackEnd;
+using LitJson;
 using UnityEngine;
 
 public class UserInfo : MonoBehaviour {
 
-    [System.Serializable]
+    [Serializable]
     public class UserInfoEvent : UnityEngine.Events.UnityEvent { }
+    
     public UserInfoEvent onUserInfoEvent = new UserInfoEvent();
 
     private static UserInfoData data = new UserInfoData();
-    public static UserInfoData Data => data;
-}
+    public static UserInfoData Data => data; // getter
 
-public interface IAbc {
+    public void GetUserInfoFromBackend() {
+        Backend.BMember.GetUserInfo(callback => {
+            if(callback.IsSuccess()) {
+                try { // JSON 데이터 파싱 성공
+                    JsonData json = callback.GetReturnValuetoJSON()["row"];
+                    data.gamerId = json["gamerId"].ToString();
+                    data.countryCode = json["countryCode"]?.ToString();
+                    data.nickname = json["nickname"]?.ToString();
+                    data.inDate = json["inDate"].ToString();
+                    data.emailForFindPassword = json["emailForFindPassword"]?.ToString();
+                    data.subscriptionType = json["subscriptionType"].ToString();
+                    data.federationId = json["federationId"]?.ToString();
+
+                } catch(Exception e) { // JSON 데이터 파싱 실패
+                    data.Reset(); // 유저 정보를 기본 상태로 설정
+                    Debug.LogError(e); // try-catch 에러 출력
+                }
+                
+            } else {
+                data.Reset(); // 유저 정보를 기본 상태로 설정
+                Debug.LogError(callback.GetMessage()); // Tip. 일반적으로 오프라인 상태를 대비해 기본적인 정보를 저장해두고 오프라인일때 불러와서 사용
+            }
+            onUserInfoEvent?.Invoke(); // 유저 정보 불러오기가 완료되었을때 onUserInfoEvent에 등록되어 있는 이벤트 메소드 호출
+        });
+    }
     
 }
 
