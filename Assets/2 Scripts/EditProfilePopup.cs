0cac39068392b3a00ad44ad61bb601744f872064
Juklim <hyui10187@naver.com>
2024-05-10 23:28:33 +0900

    유저 닉네임 수정 기능추가

---
+++ Assets/2 Scripts/EditProfilePopup.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EditProfilePopup : MonoBehaviour {
+
+    [SerializeField]
+    private Text nicknameText;
+    
+    [SerializeField]
+    private Text gamerIdText;
+
+    public void UpdateNickname() {
+        nicknameText.text = UserInfo.Data.nickname == null ? UserInfo.Data.gamerId : UserInfo.Data.nickname;
+    }
+
+}
