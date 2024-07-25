6365e9033c55b908f4a1f49f51c24fd2130bf85e
Juklim <hyui10187@naver.com>
2024-07-26 02:15:07 +0900

    초기화면 추가

---
+++ Assets/2 Scripts/00 First Scene/FirstSceneManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FirstSceneManager : MonoBehaviour
+{
+
+    public void OnLoginButtonClick() // 로그인하기 버튼을 클릭했을 경우 호출되는 메소드
+    {
+        SceneManager.LoadScene("LoginScene"); // 로그인 씬으로 이동하기
+    }
+    
+    public void OnImmediateButtonClick() // 바로하기 버튼을 클릭했을 경우 호출되는 메소드
+    {
+        SceneManager.LoadScene("MainScene"); // 로그인 씬으로 이동하기
+    }
+    
+}
