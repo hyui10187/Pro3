c75bafdba5b9e3b5329316bd429dcdeb83cd1c25
Juklim <hyui10187@naver.com>
2024-07-27 22:57:32 +0900

    사운드 시스템 추가 리팩터링

--- Assets/2 Scripts/11 Manager/SoundManager.cs
+++ Assets/2 Scripts/11 Manager/SoundManager.cs
@@ -59,9 +59,8 @@ public class SoundManager : MonoBehaviour
         audioSourceB.playOnAwake = false;
     }
 
-    private void PlaySound(AudioClipName audioClipName)
+    public void PlaySound(AudioClipName audioClipName, bool loop = false)
     {
-
         if(audioSourceQueue.Count < 1) // 모든 Audio Source가 사용중인 상태이면
         {
             GenerateAudioSource(); // 새로운 Audio Source를 생성해주기
@@ -69,10 +68,26 @@ public class SoundManager : MonoBehaviour
         
         AudioSource remainAudioSource = audioSourceQueue.Dequeue();
         remainAudioSource.clip = audioClips[(int)audioClipName];
+        remainAudioSource.loop = loop;
         remainAudioSource.gameObject.SetActive(true);
         remainAudioSource.Play();
-        
-        StartCoroutine(RetrieveAudioSource(remainAudioSource, audioClips[(int)audioClipName]));
+
+        if(!loop) // 반복 재생이 아닌 경우에만 자동으로 Audio Source를 꺼주기
+            StartCoroutine(RetrieveAudioSource(remainAudioSource, audioClips[(int)audioClipName]));
+    }
+    
+    public void StopSound()
+    {
+        AudioSource[] childAudioSource = GetComponentsInChildren<AudioSource>();
+        for(int i = 0; i < childAudioSource.Length; i++)
+        {
+            if(childAudioSource[i].loop && childAudioSource[i].gameObject.activeSelf)
+            {
+                childAudioSource[i].loop = false;
+                childAudioSource[i].gameObject.SetActive(false);
+                audioSourceQueue.Enqueue(childAudioSource[i]);
+            }
+        }
     }
 
     private IEnumerator RetrieveAudioSource(AudioSource audioSource, AudioClip audioClip)
@@ -88,112 +103,4 @@ public class SoundManager : MonoBehaviour
         audioSourceQueue.Enqueue(newAudioSource);
     }
 
-    public void PlayKeySound() {
-        audioSourceA.PlayOneShot(keySound);
-    }
-    
-    public void PlayQuitSound() {
-        audioSourceA.PlayOneShot(quitSound);
-    }
-
-    public void PlayPanelSound() {
-        audioSourceA.PlayOneShot(panelSound);
-    }
-
-    public void PlayQuestSound() {
-        audioSourceA.PlayOneShot(questSound);
-    }
-    
-    public void PlayEquipSound() {
-        audioSourceA.PlayOneShot(equipSound);
-    }
-
-    public void PlayConsumeSound() {
-        audioSourceA.PlayOneShot(consumeSound);
-    }
-
-    public void PlayPlusSound() {
-        audioSourceA.PlayOneShot(plusSound);
-    }
-
-    public void PlayBuySound() {
-        audioSourceA.PlayOneShot(buySound);
-    }
-    
-    public void PlayClickSound() {
-        audioSourceA.PlayOneShot(clickSound);
-    }
-    
-    public void PlayTalkSound() {
-        audioSourceA.PlayOneShot(talkSound);
-    }
-    
-    public void PlayWaterSound() {
-        audioSourceB.clip = waterSound;
-        audioSourceB.Play();
-        audioSourceB.loop = true;
-    }
-
-    public void StopWaterSound() {
-        audioSourceB.Stop();
-        audioSourceB.clip = null;
-        audioSourceB.loop = false;
-    }
-    
-    public void PlayFireplaceSound() {
-        audioSourceB.clip = fireplaceSound;
-        audioSourceB.Play();
-        audioSourceB.loop = true;
-    }
-
-    public void StopFireplaceSound() {
-        audioSourceB.Stop();
-        audioSourceB.clip = null;
-        audioSourceB.loop = false;
-    }
-
-    public void PlayAttackSound() {
-        audioSourceA.PlayOneShot(attackSound);
-    }
-
-    public void PlayAcquisitionSound() {
-        audioSourceA.PlayOneShot(acquisitionSound);
-    }
-    
-    public void PlayClockSound() {
-        audioSourceA.clip = clockSound;
-        audioSourceA.Play();
-        audioSourceA.loop = true;
-    }
-    
-    public void StopClockSound() {
-        audioSourceA.Stop();
-        audioSourceA.clip = null;
-        audioSourceA.loop = false;
-    }
-    
-    public void PlayDeadSound() {
-        audioSourceA.PlayOneShot(deadSound);
-    }
-
-    public void PlayDoorSound() {
-        audioSourceA.PlayOneShot(doorSound);
-    }
-    
-    public void PlayHealSound() {
-        audioSourceA.PlayOneShot(healSound);
-    }
-    
-    public void PlayLevelUpSound() {
-        audioSourceA.PlayOneShot(levelUpSound);
-    }
-    
-    public void PlayAlertSound() {
-        audioSourceA.PlayOneShot(alertSound);
-    }
-    
-    public void PlayStairSound() {
-        audioSourceA.PlayOneShot(stairSound);
-    }
-    
 }
