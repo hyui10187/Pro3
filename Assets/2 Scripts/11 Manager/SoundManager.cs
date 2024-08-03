8b65e638801a45a27d97386e78927523b7d75167
Juklim <hyui10187@naver.com>
2024-08-04 01:40:51 +0900

    코드 정리

--- Assets/2 Scripts/11 Manager/SoundManager.cs
+++ Assets/2 Scripts/11 Manager/SoundManager.cs
@@ -3,69 +3,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum AudioClipName
-{
-    Attack, Acquisition, Clock, Dead, Door, Heal, LevelUp, Alert, Talk, Stair, FirePlace,
-    Water, Click, Buy, Plus, Consume, Equip, Quest, Panel, Quit, Key
-}
+    public enum AudioClipName
+    {
+        Attack, Acquisition, Clock, Dead, Door, Heal, LevelUp, Alert, Talk, Stair, FirePlace,
+        Water, Click, Buy, Plus, Consume, Equip, Quest, Panel, Quit, Key
+    }
 
 public class SoundManager : MonoBehaviour
 {
-
     public static SoundManager instance;
 
-    [SerializeField]
-    private AudioClip[] audioClips;
-    
     private Queue<AudioSource> audioSourceQueue;
     
-    [SerializeField]
-    private AudioSource audioSourcePrefab;
-    
-    public AudioSource audioSourceA;
-    public AudioSource audioSourceB;
-    
-    public AudioClip attackSound;
-    public AudioClip acquisitionSound;
-    public AudioClip clockSound;
-    public AudioClip deadSound;
-    public AudioClip doorSound;
-    public AudioClip healSound;
-    public AudioClip levelUpSound;
-    public AudioClip alertSound;
-    public AudioClip talkSound;
-    public AudioClip stairSound;
-    public AudioClip fireplaceSound;
-    public AudioClip waterSound;
-    public AudioClip clickSound;
-    public AudioClip buySound;
-    public AudioClip plusSound;
-    public AudioClip consumeSound;
-    public AudioClip equipSound;
-    public AudioClip questSound;
-    public AudioClip panelSound;
-    public AudioClip quitSound;
-    public AudioClip keySound;
+    [SerializeField] private AudioClip[] audioClips;
+    [SerializeField] private AudioSource audioSourcePrefab;
 
     private void Awake()
     {
         instance = this;
         DontDestroyOnLoad(gameObject);
         audioSourceQueue = new Queue<AudioSource>();
-
-        audioSourceA = gameObject.AddComponent<AudioSource>();
-        audioSourceB = gameObject.AddComponent<AudioSource>();
-        audioSourceA.playOnAwake = false;
-        audioSourceB.playOnAwake = false;
     }
 
     public void PlaySound(AudioClipName audioClipName, bool loop = false)
     {
         if(audioSourceQueue.Count < 1) // 모든 Audio Source가 사용중인 상태이면
-        {
             GenerateAudioSource(); // 새로운 Audio Source를 생성해주기
-        }
-        
+
         AudioSource remainAudioSource = audioSourceQueue.Dequeue();
         remainAudioSource.clip = audioClips[(int)audioClipName];
         remainAudioSource.loop = loop;
