a17bd0c89276943578cdadd17a5e5c58397e0a04
hyui10187 <hyui10187@naver.com>
2024-08-14 23:00:58 +0900

    코드 정리

--- Assets/02 Scripts/10 Backend/BackendManager.cs
+++ Assets/02 Scripts/10 Backend/BackendManager.cs
@@ -3,21 +3,23 @@ using BackEnd;
 using TMPro;
 using UnityEngine.UI; // 뒤끝 SDK
 
-public class BackendManager : MonoBehaviour {
+public class BackendManager : MonoBehaviour
+{
     
-    private void Awake() {
+    private void Awake()
+    {
         DontDestroyOnLoad(gameObject); // Update 메소드의 Backend.AsyncPoll 호출을 위해 파괴하지 않는다
         BackendSetup(); // 뒤끝 서버 초기화
     }
 
-    private void Update() {
-        if(Backend.IsInitialized) {
+    private void Update()
+    {
+        if(Backend.IsInitialized) 
             Backend.AsyncPoll();
-            
-        }
     }
 
-    private void BackendSetup() {
+    private void BackendSetup()
+    {
         BackendReturnObject bro = Backend.Initialize(true); // 뒤끝 초기화
 
         if(bro.IsSuccess()) { // 뒤끝 초기화에 대한 응답값
@@ -25,7 +27,6 @@ public class BackendManager : MonoBehaviour {
         } else {
             Debug.Log($"초기화 실패 : {bro}"); // 초기화 실패시 statusCode 400대 에러 발생
         }
-
     }
-    
+
 }
