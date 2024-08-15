911c9e2027fae0b26bd0460525115a6c10d2e75f
hyui10187 <hyui10187@naver.com>
2024-08-15 22:22:14 +0900

    코드 정리

--- Assets/02 Scripts/EquipmentSlot.cs
+++ Assets/02 Scripts/EquipmentSlot.cs
@@ -4,7 +4,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class EquipmentSlot : MonoBehaviour {
+public class EquipmentSlot : MonoBehaviour
+{
     
     public static EquipmentSlot instance;
     
@@ -12,25 +13,29 @@ public class EquipmentSlot : MonoBehaviour {
     public Image itemImage;
     public ItemName itemName;
 
-    private void Awake() {
+    private void Awake()
+    {
         instance = this;
     }
 
-    public void ButtonDown() { // 장비창의 장착된 아이템 슬롯을 클릭해서 장비를 해제하는 메소드
+    public void ButtonDown() // 장비창의 장착된 아이템 슬롯을 클릭해서 장비를 해제하는 메소드
+    {
 
-        for(int i = 0; i < Inventory.instance.possessItems.Count; i++) {
-            if(InventoryManager.instance.inventorySlots[i].item.itemName == itemName) {
+        for(int i = 0; i < Inventory.instance.possessItems.Count; i++)
+        {
+            if(InventoryManager.instance.inventorySlots[i].item.itemName == itemName)
+            {
                 InventoryManager.instance.inventorySlots[i].equipImage.SetActive(false); // 장착중이라는 E 아이콘을 꺼주기
 
-                switch(itemType) {
-                    
+                switch(itemType)
+                {
                     case ItemType.Weapon: // 무기를 해제했으면 equipSword 플래그값 내려주기
                         
-                        if(itemName == ItemName.소드) {
+                        if(itemName == ItemName.소드)
                             Inventory.instance.equipSword = false;
-                        } else if(itemName == ItemName.활) {
+
+                        else if(itemName == ItemName.활)
                             Inventory.instance.equipBow = false;
-                        }
                         
                         GameManager.instance.itemAttackPower = 0;
                         break;
@@ -46,11 +51,13 @@ public class EquipmentSlot : MonoBehaviour {
         }
     }
 
-    public void RemoveSlot() {
+    public void RemoveSlot()
+    {
         itemImage.gameObject.SetActive(false); // 슬롯의 아이템 이미지 꺼주기
     }
 
-    public void RedrawSlot(Item item) { // 장비창의 슬롯을 다시 그려주는 메소드
+    public void RedrawSlot(Item item) // 장비창의 슬롯을 다시 그려주는 메소드
+    {
         itemImage.sprite = item.itemImage;
         itemImage.gameObject.SetActive(true);
         itemName = item.itemName;
