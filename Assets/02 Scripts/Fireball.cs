9f154c60676892b98c0a792d13ea17b479bf3fa4
hyui10187 <hyui10187@naver.com>
2024-08-22 23:37:04 +0900

    코드 정리

--- Assets/02 Scripts/Fireball.cs
+++ Assets/02 Scripts/Fireball.cs
@@ -3,31 +3,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Fireball : MonoBehaviour {
-
+public class Fireball : MonoBehaviour
+{
     public int damage; // 파이어볼의 공격력
     public float speed; // 파이어볼의 속도
 
     private Rigidbody2D rigid;
 
-    private void Awake() {
+    private void Awake()
+    {
         rigid = GetComponent<Rigidbody2D>();
     }
 
-    public void Shot(Vector3 dir) {
+    public void Shot(Vector3 dir)
+    {
         rigid.velocity = dir * speed;
         
         Invoke("DestroyFireball", 7f); // 파이어볼이 아디에도 닿지 않아도 7초가 지나면 자동으로 없애주기
     }
 
-    private void OnTriggerEnter2D(Collider2D other) {
-
-        if(other.CompareTag("Player") || other.CompareTag("Wall")) { // 파이어볼이 플레이어한테 닿거나 벽에 닿으면
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player") || other.CompareTag("Wall")) // 파이어볼이 플레이어한테 닿거나 벽에 닿으면
+        {
             Destroy(gameObject); // 파이어볼 삭제
         }
     }
 
-    private void DestroyFireball() {
+    private void DestroyFireball()
+    {
         Destroy(gameObject);
     }
     
