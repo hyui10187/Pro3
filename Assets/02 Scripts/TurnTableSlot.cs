93f2879fad6bd8fa92ad07e2cecf8a357b9da084
hyui10187 <hyui10187@naver.com>
2024-08-24 22:20:09 +0900

    코드 정리

--- Assets/02 Scripts/TurnTableSlot.cs
+++ Assets/02 Scripts/TurnTableSlot.cs
@@ -3,11 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TurnTableSlot : MonoBehaviour {
-
+public class TurnTableSlot : MonoBehaviour
+{
     public int recordNum;
 
-    public void PlayTurnTable() {
+    public void PlayTurnTable()
+    {
         TurnTableManager.instance.PlayTurnTable(recordNum);
         SoundManager.instance.PlaySound(AudioClipName.Click);
     }
