e3fc5837481b7aae1be52b56c83ed753b1a03f93
hyui10187 <hyui10187@naver.com>
2024-08-31 20:24:11 +0900

    코드 정리

--- Assets/02 Scripts/ObjData.cs
+++ Assets/02 Scripts/ObjData.cs
@@ -2,9 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjData : MonoBehaviour {
-
+public class ObjData : MonoBehaviour
+{
     public int objId;
     public bool isNpc;
-    
 }
