4151092d04a10833f1cab33f87edfb21dae5efde
hyui10187 <hyui10187@naver.com>
2024-09-07 00:53:32 +0900

    코드 정리

--- Assets/02 Scripts/14 Item/ItemManager.cs
+++ Assets/02 Scripts/14 Item/ItemManager.cs
@@ -40,25 +40,35 @@ public class ItemManager : MonoBehaviour
         itemScript.SetItem(itemDB[Random.Range(0, 5)]);
     }
 
-    public void DropGold(Vector3 dropPos) {
+    public void DropGold(Vector3 dropPos)
+    {
         GameObject obj = Instantiate(itemPrefab, dropPos, Quaternion.identity, fieldItemParent.transform);
         obj.transform.localScale *= 3f;
         ItemScript itemScript = obj.GetComponent<ItemScript>();
         itemScript.SetItem(goldDB[Random.Range(0, 3)]);
     }
     
-    public void DropFruit(Vector3 dropPos) {
+    public void DropFruit(Vector3 dropPos)
+    {
         GameObject obj = Instantiate(itemPrefab, dropPos, Quaternion.identity, fieldItemParent.transform);
         obj.transform.localScale *= 3f; // Item Prefab의 크기가 작으니 그거의 3배를 곱해줘서 적절한 크기로 생성하기
         ItemScript itemScript = obj.GetComponent<ItemScript>();
         itemScript.SetItem(fruitDB[Random.Range(0, 3)]); // 0, 1, 2 열매 중에서 무작위로 드랍하도록
     }
     
-    public void DropMaterial(Vector3 dropPos, int idx) {
+    public void DropMaterial(Vector3 dropPos, int idx)
+    {
         GameObject obj = Instantiate(itemPrefab, dropPos, Quaternion.identity, fieldItemParent.transform);
         obj.transform.localScale *= 2f;
         ItemScript itemScript = obj.GetComponent<ItemScript>();
         itemScript.SetItem(materialDB[idx]); // 파라미터로 받은 idx 값에 맞는 아이템을 생성하도록
+    }
+    
+    public void DropEnchant(Vector3 dropPos, int idx) // 강화석을 드롭하는 메소드
+    {
+        GameObject obj = Instantiate(itemPrefab, dropPos, Quaternion.identity, fieldItemParent.transform);
+        ItemScript itemScript = obj.GetComponent<ItemScript>();
+        itemScript.SetItem(enchantDB[idx]); // 파라미터로 받은 idx 값에 맞는 아이템을 생성하도록
     }
     
 }
